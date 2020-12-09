e25d492754f33413ffbe2ba67864a99802f3e6ff
Jainbhavik332 <73811833+Jainbhavik332@users.noreply.github.com>
2020-12-09 10:12:49 +0530

    DATABASE CONNECTIVITY

---
+++ pdo.php
@@ -0,0 +1,5 @@
+<?php 
+    $pdo = new PDO('mysql:host=localhost; dname=id15556136_signup','id15556136_bhavik','Jainbhavik332*');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
+
